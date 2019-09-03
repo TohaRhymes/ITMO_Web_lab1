5683645ed73d5db101c6cd8fecff68d660c3f6fc
TohaRhymes <anton.chana@gmail.com>
2019-09-03 12:22:01 +0300

    v1.02

--- WebLab1/select.php
+++ WebLab1/select.php
@@ -10,12 +10,22 @@
             text-align: center;
         }
 
+        #scriptError {
+            font-family: "Arial", sans-serif;
+            font-style: normal;
+            font-size: 150%;
+            color: red;
+            text-align: center;
+
+        }
+
         .img {
             text-align: center;
         }
     </style>
 </head>
 <body>
+
 <?php
 $start = microtime(true);
 date_default_timezone_set("UTC");
@@ -24,14 +34,28 @@ echo "<p id='script'>Текущее время: " . date("H:i:s",
         $time) . "</p>";
 
 if (empty($_GET['X']) || empty($_GET['Y']) || empty($_GET['R']))
-    die ("Вы не выбрали все переменные");
+    die ('<p id = \'scriptError\'>Вы не выбрали все переменные o_0</p>');
 $x = $_GET['X'];
 $y = $_GET['Y'];
 $r = $_GET['R'];
-if(!preg_match('/(?<![\.\d])\d+(?![\.\d])/', $x)||!is_numeric($y)||!preg_match('/(?<![\.\d])\d+(?![\.\d])/', $r)||$x<-4||$x>4||$y<-5||$y>3||$r<1||$r>5)
-    die ("X и R должны быть целыми числами, x от -4 до 4, r от 1 до 5. Y числом  от -5 до 3.");
+if (!preg_match('/(?<![\.\d])\d+(?![\.\d])/', $x) || !is_numeric($y) || !preg_match('/(?<![\.\d])\d+(?![\.\d])/', $r) || $x < -4 || $x > 4 || $y < -5 || $y > 3 || $r < 1 || $r > 5)
+    die ("<p id = 'scriptError'>ATTENTION! o_0</p> <p id = 'scriptError'>X и R должны быть целыми числами, x от -4 до 4, r от 1 до 5. Y числом  от -5 до 3.</p>");
 
 if ($y >= 0 && $x >= 0 && $y + 2 * $x <= $r || $y >= 0 && $x < 0 && $y ** 2 + $x ** 2 <= $r ** 2 || $y < 0 && $x < 0 && $y >= -$r / 2 && $x >= -$r) {
+//    echo '<table id = "script">
+//        <tr>
+//            <th>X</th>
+//            <th>Y</th>
+//            <th>R</th>
+//            <th>Попадает/нет</th>
+//        </tr>
+//        <tr>
+//            <th>' . $x . '</th>
+//            <th>' . $y . '</th>
+//            <th>' . $r . '</th>
+//            <th>НЕТ</th>
+//        </tr>
+//    </table>';
     echo '<p id = "script"> Точка (' . $x . '; ' . $y . ') при параметре R = ' . $r . ' попала в закрашенную область!
                     <figure class="img">
                         <img height="130px"
@@ -41,7 +65,20 @@ if ($y >= 0 && $x >= 0 && $y + 2 * $x <= $r || $y >= 0 && $x < 0 && $y ** 2 + $x
                     </figure>
                 </p>';
 } else {
-
+//    echo '<table id = "script">
+//        <tr>
+//            <th>X</th>
+//            <th>Y</th>
+//            <th>R</th>
+//            <th>Попадает/нет</th>
+//        </tr>
+//        <tr>
+//            <th>' . $x . '</th>
+//            <th>' . $y . '</th>
+//            <th>' . $r . '</th>
+//            <th>НЕТ</th>
+//        </tr>
+//    </table>';
     echo '<p id = "script"> Точка (' . $x . '; ' . $y . ') при параметре R = ' . $r . ' не попала в закрашенную область!
                     <figure class="img">
                         <img height="130px"
